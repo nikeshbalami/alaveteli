e91cc19f87fc8fd3cd3822e2a0041ed8eb2b30ac
lizconlan <liz@mysociety.org>
2017-01-17 11:45:44 +0000

    Extract dormant user query into a lib function

--- lib/user_stats.rb
+++ lib/user_stats.rb
@@ -27,4 +27,30 @@ class UserStats
     User.connection.select_all(sql)
   end
 
+  # Returns the number of domant users for the given domain
+  # (A dormant user is one with no requests, tracks or comments)
+  def self.count_dormant_users(domain, start_date=nil)
+    # When we have Rails 4 across the board, we get to say "where.not" and
+    # rewrite this using the ORM
+    # example code here: http://stackoverflow.com/a/23389130), until then...
+    #
+    # Reminder - check that the returned ids in the subquery does not include
+    # null values otherwise this will unexpectedly return 0
+    # (see http://stackoverflow.com/a/19528722) this should not be a thing but
+    # is happening on WDTK with the info_requests table for some reason
+    sql = "SELECT count(*) FROM users " \
+          "WHERE id NOT IN ( " \
+          "  SELECT DISTINCT user_id FROM info_requests " \
+          "  WHERE user_id IS NOT NULL " \
+          ") AND id NOT IN ( " \
+          "  SELECT DISTINCT tracking_user_id FROM track_things " \
+          "  WHERE tracking_user_id IS NOT NULL " \
+          ") AND id NOT IN ( " \
+          "  SELECT DISTINCT user_id FROM comments " \
+          "  WHERE user_id IS NOT NULL " \
+          ") AND email LIKE '%@#{domain}'"
+    sql += "AND created_at >= '#{start_date}'" if start_date
+    User.connection.select_all(sql).first["count"].to_i
+  end
+
 end
